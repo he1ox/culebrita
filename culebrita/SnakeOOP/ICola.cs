33dc82729a218678b7f4b75a72a2e79fc5912ad6
Jorge Lopez <jlopezgarciagt@gmail.com>
2022-05-26 20:16:14 -0600

    interface for all queue implementations

---
+++ culebrita/SnakeOOP/ICola.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace culebrita.SnakeOOP
+{
+    internal interface ICola
+    {
+        /// <summary>
+        /// Este método se encarga de insertar un elemento a la cola
+        /// </summary>
+        /// <param name="elemento">Objeto a encolar</param>
+        void EnQueue(Object elemento);
+
+        /// <summary>
+        /// Este método se encarga de eliminar el primer elemento en la cola, y lo devuelve. 
+        /// </summary>
+        /// <returns>Objeto</returns>
+        object DeQueue();
+
+        /// <summary>
+        /// Este método resetea la cola.
+        /// </summary>
+        void BorrarCola();
+
+        /// <summary>
+        /// Este método devuelve el primer elemento en haber entrado a la cola.
+        /// </summary>
+        void FrenteCola();
+        /// <summary>
+        /// Verifica si la colá esta vacía.
+        /// </summary>
+        /// <returns>True</returns>
+        bool ColaVacia();
+
+        /// <summary>
+        /// Verifica si la cola está llena.
+        /// </summary>
+        /// <returns>True</returns>
+        bool ColaLlena();
+    }
+}
